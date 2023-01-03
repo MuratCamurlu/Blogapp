addf2801c92587e60634eedfb35b23c085247dcb
MuratÇamurlu <mcamurlu1923@gmailcom>
2023-01-03 16:16:56 +0300

    user auth

--- main/settings.py
+++ main/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # blog
     'blog',
+    'user',
     #third party
     'rest_framework',
     'django_filters',
@@ -129,5 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend', 'rest_framework.filters.SearchFilter']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend', 'rest_framework.filters.SearchFilter'],
+     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication']
 }
--- user/views.py
+++ user/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.generics import CreateAPIView
+from django.contrib.auth.models import User
+from .serializers import RegistrationSerializer
+class RegisterView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegistrationSerializer
